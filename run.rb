e099833cfdeffb6a03087015ae1fa6e03a65ff05
dexy <dexy.31337@gmail.com>
2021-03-28 22:12:09 +0200

    Добавил команду на компетатив карты

--- run.rb
+++ run.rb
@@ -2,11 +2,20 @@
 
 require 'logger'
 require 'telegram_bot'
-
+=begin
+maps_all - Выбор из 18 карт
+maps_comp - Выбор из 18 карт
+callme - Вписаться в оповещения
+fuckoff - Выписаться из оповещений
+list - Список оповещаемых
+iwannaplay - ГО КАТАц!
+cocktail - Коктейль
+=end
 TELEGRAM_BOT_TOKEN = ENV['TOKEN'] 
 
 bot = TelegramBot.new(token: TELEGRAM_BOT_TOKEN, logger: Logger.new(STDOUT))
 maps = ['Ancient','Anubis','Cache','Canals','Cobblestone','D2','Engage','Inferno','Mirage','Nuke','Overpass','Train','Vertigo','Agency','Apollo','Italy','Militia','Office']
+comp_maps = ['D2','Inferno','Mirage','Nuke','Overpass','Train','Vertigo']
 cocktails = ['Молик тебе в темку','Две хаешки на твой банан','Засмочил тебе туалеты','А не сгоришь в молике?','Сейчас я им буст развалю','ХЕ себе под ноги кинь!','Выходи так, там никого','Я белый...','Даю декой в коннектор','Спрей контролить научись, потом проси','Может тебе еще хед флешкой поставить?','Попикай пока, а там посмотрим','А без флешки слабо выйти?']
 
 usernames = []
@@ -31,7 +40,9 @@ bot.get_updates(fail_silently: true) do |message|
     when /start/i
       reply.text = "Готов хуячить, #{message.from.first_name}!"
     when /maps_all/i
-      reply.text = maps[rand(maps.length)]
+      reply.text = "Го катнём на #{maps[rand(maps.length)]}"
+    when /maps_comp/i
+      reply.text = "Го катнём на #{comp_maps[rand(comp_maps.length)]}"
     when /callme/i
       if (message.from.username == "")
         reply.text = "У тебя нет юзернейма #{message.from.first_name} :( Не могу"
